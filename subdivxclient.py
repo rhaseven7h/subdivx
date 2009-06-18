e28962ec4e474d00bc80f716028ee556dd05e2c1
Gabriel Medina <rha7.com@gmail.com>
2009-06-17 21:18:36 -0600

    Added python version of subdivx

---
+++ subdivxclient.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+#
+#   This file is part of SubDivX.
+#
+#   SubDivX is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License.
+#
+#   SubDivX is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with SubDivX, if not, see <http://www.gnu.org/licenses/>.
+#
+#   See the LICENSE file.
+#
+
+import re
+import urllib
+import urllib2
+import lxml.html
+import os.path
+import tempfile
+
+class SubDivXClient:
+  """
+  Client for spanish subtitles downloading from http://www.subdivx.com/
+
+    
+  Examples:
+  
+  # List of dictionaries of results [ {'title': title, 'link': link, 'description': description, 'cds': cds}, ... ]
+  subs = SubDivXClient.search('The Matrix').get_results()
+  
+  # Path to downloaded sutitles archive file, i.e. /tmp/subdivx/32425.rar
+  filepath = SubDivXClient().search('The Matrix').fecth(1) 
+  
+  """
+  
+  def __init__(self):
+    self.results = []
+    
+  def get_results(self):
+    """
+    Returns the results list
+    [ {'title': title, 'link': link, 'description': description, 'cds': cds}, ... ]
+    """
+    return self.results
+
+  def search(self, str_terms, page):
+    """
+    Makes a search for subtitles.
+    Returns self for chaining.
+    
+    str_terms -- The string to search for at subdivx.com
+    page -- The page number to fetch
+    """
+    
+    # The magic url for searching
+    str_url = "http://www.subdivx.com/index.php?buscar=%s&accion=5&masdesc=1&subtitulos=1&realiza_b=1" % urllib.quote(str_terms)
+    
+    # If not the first page, add the page parameter.
+    if page > 1:
+      str_url += "&pg=" +str(page)
+    
+    # Fetch the search results page
+    doc = lxml.html.fromstring(unicode(urllib.urlopen(str_url).read(), errors='ignore')).getroottree().getroot()
+    
+    # Initialize the result elements, separate parallel lists of data
+    titles = []
+    links = []
+    descriptions = []
+    cds = []
+
+    # Fetch the titles and links
+    for link in doc.cssselect("a.titulo_menu_izq"):
+      titles.append(link.text_content())
+      links.append(link.get('href'))
+
+    # Now the descriptions
+    for desc in doc.cssselect("div#buscador_detalle_sub"):
+      descriptions.append(desc.text_content())
+      
+    # The number of cds ...
+    for dato in doc.cssselect("div#buscador_detalle_sub_datos"):
+      cds.append(re.search('Cds:<\/b> ([0-9]+)', lxml.etree.tostring(dato)).group(1))
+      
+    # We will consolidate the above results in a list of dictionaries in the results list
+    self.results = []
+    for i in range(len(titles)):
+      self.results.append({ 'title': titles[i], 'link': links[i], 'description': descriptions[i], 'cds': cds[i] })
+
+    # And return self, for chaining, see SubDivXClient class doc.
+    return self
+    
+  def fetch(self, int_num):
+    """
+    Fetches the selected result from results (which you fill up by calling search()),
+    and returns the path to the downloaded file stored at {SYSTEM_TEMP_DIR}/subdivx/
+    You MUST call search first.
+    
+    int_num -- The results index number, starting from 0 (zero), from results array
+    """
+    
+    # Create a request object for the result page with the captcha form for the selected result.
+    # From here we will get the 'desub' and 'u' paramaters needed to make the request for
+    # the actual file.
+    # Must make a req. first, because we need to add a Referer header.
+    req = urllib2.Request(self.results[int_num]['link'])
+    req.add_header('Referer', self.results[int_num]['link'])
+    
+    # Fetch the above request and parse it.
+    doc = lxml.html.fromstring(unicode(urllib2.urlopen(req).read(), errors='ignore')).getroottree().getroot()
+    
+    # Get the 'desub' and 'u' form parameters
+    desub = doc.cssselect("input[type='hidden'][name='desub']")[0].get('value')
+    u = doc.cssselect("input[type='hidden'][name='u']")[0].get('value')
+
+    # Now build a second request to fetch the actual file, as before we need a req. object
+    # to add the Referer header, now the referer is the above fetched url.
+    req = urllib2.Request("http://www.subdivx.com/bajar.php?captcha_user=ME2&idcaptcha=1007&desub=%s&u=%s" % (desub, u))
+    req.add_header('Referer', self.results[int_num]['link'])
+    
+    # And open the subtitle actual file url
+    subs = urllib2.urlopen(req)
+    
+    # Get the content-disposition header to obtain the file name
+    # subdivx.com returns the subtitles as rars and zips, so we
+    # need to know the name of the file.
+    fn = subs.info().get('content-disposition').split(';')[1].split('=')[1]
+    
+    # Create the output temporary directory 'subdivx' below the system's
+    # temporary directory. If it doesn't exist, create it.
+    out_dir = os.path.join(tempfile.gettempdir(), 'subdivx')
+    if os.path.exists(out_dir):
+      if os.path.isdir(out_dir):
+        pass
+      else:
+        raise IOError("Temp file subdivx couldn't be created or already exists as a file or with wrong permissions")
+    else:
+      os.path,makedirs(out_dirs)
+      
+    # Now we know we have the subdirectory to hold the file, generate the file name
+    # using the above temporary directory and the file name we got from the content-disposition
+    # header
+    out_file = os.path.join(out_dir, fn)
+
+    # Open in binary write mode the output file, and write to it the data
+    # read from the url.
+    of = open(out_file, 'wb')
+    of.write(subs.read())
+    
+    # Clean up our mess
+    of.close()
+    subs.close()
+    
+    # And return the saved subtitle file path.
+    return out_file
+
---
+++ subdivxgui.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+#
+#   This file is part of SubDivX.
+#
+#   SubDivX is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License.
+#
+#   SubDivX is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with SubDivX, if not, see <http://www.gnu.org/licenses/>.
+#
+#   See the LICENSE file.
+#
+
+import os
+import gtk
+import urllib
+import subdivxclient
+
+class SubDivXGUI:
+  def __init__(self):
+    self.subdivxclient = subdivxclient.SubDivXClient()
+    self.builder = gtk.Builder()
+    self.builder.add_from_file('subdivx.glade')
+    self.builder.connect_signals({
+      'on_button_quit_clicked'         : self.app_quit,
+      'on_window_subdivx_destroy'      : self.app_quit,
+      'on_button_search_clicked'       : self.on_button_search_clicked,
+      'on_button_next_clicked'         : self.on_button_next_clicked,
+      'on_button_clear_clicked'        : self.on_button_clear_clicked,
+      'on_button_results_fetch_clicked': self.on_button_results_fetch_clicked
+    })
+    
+    self.win = self.builder.get_object('window_subdivx')
+    self.win.maximize()
+    
+    self.entry_search = self.builder.get_object("entry_search")
+
+    self.treeview_results = self.builder.get_object("treeview_results")
+
+    column = gtk.TreeViewColumn('Name', gtk.CellRendererText(), text = 1)
+    column.expand = False
+    self.treeview_results.append_column(column)
+
+    column = gtk.TreeViewColumn('CDs', gtk.CellRendererText(), text = 3)
+    column.expand = False
+    self.treeview_results.append_column(column)
+
+    column = gtk.TreeViewColumn('Description', gtk.CellRendererText(), text = 2)
+    column.expand = False
+    self.treeview_results.append_column(column)
+
+    self.treeview_results.set_tooltip_column(2)
+
+    self.liststore_results = self.builder.get_object("liststore_results")
+    
+    self.button_next = self.builder.get_object("button_next")
+    self.button_next.set_sensitive(False)
+    self.button_clear = self.builder.get_object("button_clear")
+    self.button_clear.set_sensitive(False)
+    self.button_search = self.builder.get_object("button_search")
+    self.button_search.set_sensitive(True)
+
+    self.label_results = self.builder.get_object("label_results")
+    
+    self.page = 1
+    
+  def run(self):
+    self.win.show()
+    gtk.main()
+
+  def app_quit(self, widget):
+    gtk.main_quit()
+
+  def on_button_search_clicked(self, widget):
+    if len(self.entry_search.get_text().strip()) > 0:
+      self.entry_search.set_sensitive(False)
+      self.button_search.set_sensitive(False)
+      self.button_next.set_sensitive(True)
+      self.button_clear.set_sensitive(True)
+      self.label_results.set_text("Results Page %s" % self.page)
+      self.liststore_results.clear()
+      results = self.subdivxclient.search(self.entry_search.get_text().strip(), self.page).results
+      for i in range(len(results)):
+        result = results[i]
+        iter = self.liststore_results.append()
+        self.liststore_results.set_value(iter, 0, i)
+        self.liststore_results.set_value(iter, 1, results[i]['title'])
+        self.liststore_results.set_value(iter, 2, results[i]['description'])
+        self.liststore_results.set_value(iter, 3, int(results[i]['cds']))
+        self.liststore_results.set_value(iter, 4, results[i]['link'])
+
+      if len(results) < 20:
+        self.button_next.set_sensitive(False)
+    else:
+      dialog = gtk.MessageDialog(self.win, flags=gtk.DIALOG_DESTROY_WITH_PARENT, type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_CLOSE)
+      dialog.set_title("Error Searching")
+      dialog.set_markup("Search Was Not Performed")
+      dialog.format_secondary_text("You didn't specify any search terms.\nPlease try again.")
+      dialog.run()
+      dialog.destroy()      
+
+  def on_button_next_clicked(self, widget):
+    self.page += 1
+    self.on_button_search_clicked(widget)
+  
+  def on_button_clear_clicked(self, widget):
+    self.page = 1
+    self.liststore_results.clear()
+    self.entry_search.set_sensitive(True)
+    self.button_search.set_sensitive(True)
+    self.button_next.set_sensitive(False)
+    self.button_clear.set_sensitive(False)
+    self.label_results.set_text("Results")
+  
+  def on_button_results_fetch_clicked(self, widget):
+    sel_model, sel_iter = self.treeview_results.get_selection().get_selected()
+    if sel_iter:
+      os.system("gnome-open %s" % self.subdivxclient.fetch(sel_model.get_value(sel_iter, 0)))
+    else:                                                                      
+      dialog = gtk.MessageDialog(self.win, flags=gtk.DIALOG_DESTROY_WITH_PARENT, type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_CLOSE)
+      dialog.set_title("Error Downloading")
+      dialog.set_markup("No Result Selected")
+      dialog.format_secondary_text("You have to select a result to download.\nPlease try again.")
+      dialog.run()
+      dialog.destroy()
+
+
+
+
